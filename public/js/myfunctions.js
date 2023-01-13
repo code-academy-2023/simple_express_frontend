b5e4a8301384d511e887c213402d7c3da4a23f1a
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-01-13 10:36:03 +0200

    formit page2:een

--- public/js/myfunctions.js
+++ public/js/myfunctions.js
@@ -17,3 +17,17 @@ window.onload=function loadMainPage(){
     //kutsutaan aina kun sivu ladataan
     loadPage('mainpage.html');
 }
+function showLanguage(){
+	var select=document.getElementById("source_language");
+	//console.log(select);
+	var source_language=select.value;
+	console.log(source_language);
+	var target_language=document.getElementById("target_language");
+	target_language.value=source_language;
+}
+
+function showPhone(phone){
+	console.log(phone);
+	var target_phone=document.getElementById("target_phone");
+	target_phone.value=phone;
+}
